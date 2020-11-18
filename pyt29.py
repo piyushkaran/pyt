ed6d3493aed6d1a92a28212ef504d22e03d2d2cb
piyushkaran <piyushkaran1122@gmail.com>
2020-11-18 09:54:19 +0530

    pyt 29-31

---
+++ pyt29.py
@@ -0,0 +1,5 @@
+num=int(input("Enter no."))
+if(num%2==0):
+    print("EVen")
+else:
+    print("odd")
---
+++ pyt31.py
@@ -0,0 +1,12 @@
+n=int(input())
+c=0
+if(n>1):
+    for i in range(1,n+1):
+        if(n%i)==0:
+            c=c+1
+    if(c==2):
+        print("no. is prime")
+    else:
+        print("not prime")
+else:
+    print('Enter valid')
